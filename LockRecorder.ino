bb492a7bff60c083fb559c16e3684859f5a353dd
Svante <svanteman123@yahoo.se>
2025-12-09 10:48:15 +0100

    Update LockRecorder.ino

--- LockRecorder.ino
+++ LockRecorder.ino
@@ -3,10 +3,12 @@
 void LockRecorder::unlockrecord()
 {
   unlockCount++;
+  Serial.println("Times unlocked");
   Serial.println(unlockCount);
 }
 void LockRecorder::unlockattemptRecord()
 {
   unlockAttempts++;
+  Serial.println("Times faled to unlock");
   Serial.println(unlockAttempts);
 }
